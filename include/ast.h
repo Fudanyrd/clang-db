486627af7d23fd4ebbdc26d4f0e737eb0ebd7175
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2026-05-12 02:06:11 +0000

    fix: apply design change (commit e8153aaa)

--- include/ast.h
+++ include/ast.h
@@ -22,6 +22,44 @@ struct BuildDatabaseConsumer;
 struct ClassDeclVisitor; /* internal use. */
 
 /**
+ * <h3>ReadMe - Type Column Spec</h3>
+ *
+ * The "type" column for a C++ record is "5class" or "6struct".
+ */
+inline const char *TypeofCXXRecordDecl(CXXRecordDecl *RD) {
+  const char *Type;
+  if (RD->isStruct()) {
+    Type = "6struct";
+  } else if (RD->isClass()) {
+    Type = "5class";
+  } else {
+    llvm_unreachable("Unknown CXXRecordDecl kind");
+  }
+  return Type;
+}
+
+inline const char *EncodeAccessSpecifier(AccessSpecifier Access) {
+  switch (Access) {
+  case AS_public:
+    return "6public";
+  case AS_protected:
+    return "9protected";
+  case AS_private:
+    return "7private";
+  }
+  /* AS_none: */
+  llvm_unreachable("Unknown AccessSpecifier");
+}
+
+inline std::string TypeofLocalCXXRecordDecl(CXXRecordDecl *RD,
+                                            AccessSpecifier Access) {
+  const char *Type = TypeofCXXRecordDecl(RD);
+  std::string Ret = Type;
+  Ret += EncodeAccessSpecifier(Access);
+  return Ret;
+}
+
+/**
  * Context for {@link BuildVisitor} and {@link BuildDatabaseConsumer}.
  */
 struct BuildVisitorContext {
@@ -53,7 +91,8 @@ private:
       : DB(Database), NamespaceStack(), AnonymousNamespaceCounter(0),
         CompilerContext(Ctx) {}
 
-  void EnterNamespaceOrClass(DeclContext *DeclCtx, llvm::StringRef Name) {
+  void EnterNamespaceOrClass(DeclContext *DeclCtx, llvm::StringRef Name,
+                             std::string Type) {
     auto *TopLevel = NamespaceStack.back();
     assert(TopLevel == DeclCtx->getParent() &&
            "NamespaceDecl's parent should be the current decl context");
@@ -61,20 +100,20 @@ private:
     auto CurNsWhole = CurrentNamespace();
     auto ShortNs = EncodeNs(Name);
     NamespaceStack.push_back(DeclCtx);
-    auto NewNsWhole = CurNsWhole + ShortNs;
     if (dyn_cast<CXXRecordDecl>(TopLevel)) {
-      DB->InsertIntoClass(CurNsWhole, ShortNs, NewNsWhole);
+      DB->InsertIntoClass(CurNsWhole, ShortNs, Type);
     } else {
-      DB->InsertIntoNamespace(CurNsWhole, ShortNs, NewNsWhole);
+      DB->InsertIntoNamespace(CurNsWhole, ShortNs, Type);
     }
   }
 
   void EnterNamespace(NamespaceDecl *ND) {
-    EnterNamespaceOrClass(ND, ND->getName());
+    EnterNamespaceOrClass(ND, ND->getName(), "9namespace");
   }
 
   void EnterClass(CXXRecordDecl *RD) {
-    EnterNamespaceOrClass(RD, RD->getName());
+    const char *Type = TypeofCXXRecordDecl(RD);
+    EnterNamespaceOrClass(RD, RD->getName(), Type);
   }
 
   void EnterTranslationUnit(TranslationUnitDecl *TU) {
