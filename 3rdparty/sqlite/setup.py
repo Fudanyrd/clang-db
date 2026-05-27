4a84b7d29f55a0f004a3d7a98286d0985f434f1b
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2026-05-27 03:59:34 +0000

    enhance: sqlite python installation [ci skip]

--- 3rdparty/sqlite/setup.py
+++ 3rdparty/sqlite/setup.py
@@ -1,6 +1,8 @@
 from skbuild import setup
 
 if __name__ == "__main__":
+    # ENABLE_INSTALL is turned off because
+    # header files and the static library are not needed for the Python bindings.
     setup(
         name = 'sqlite',
         packages = ['sqlite'],
@@ -9,6 +11,7 @@ if __name__ == "__main__":
         license = 'Public Domain',
         cmake_args = [
             "-DSQLITE_BUILD_PYTHON=ON",
+            "-DSQLITE_ENABLE_INSTALL=OFF",
             "-DSQLITE_ENABLE_TESTS=OFF",
         ],
     )
